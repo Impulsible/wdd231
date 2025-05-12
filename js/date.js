605b36d49419be3c30b8f960d4710f9f5531d07d
Impulsible <henryosuagwu22@gmail.com>
2025-05-12 09:10:24 +0100

    Delete js directory

--- js/date.js
+++
@@ -1,5 +0,0 @@
-// Set current year dynamically
-document.getElementById("currentYear").textContent = new Date().getFullYear();
-
-// Set last modified date dynamically
-document.getElementById("lastModified").textContent = document.lastModified;
